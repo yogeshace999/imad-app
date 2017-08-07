175d5a6f5c229a1dcc784f865769af4b9202631c
yogeshace999 <30661893+yogeshace999@users.noreply.github.com>
2017-08-07 20:35:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ app.get('/article-1', function (req, res) {
 
 });
 app.get('/article-2', function (req, res) {
-  res.send('Article 2 served and requested here');
+    res.sendFile(path.join(__dirname, 'ui', 'article-2.html'));
 });
 app.get('/article-3', function (req, res) {
-  res.send('Article 3 served and requested here');
+    res.sendFile(path.join(__dirname, 'ui', 'article-3.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
